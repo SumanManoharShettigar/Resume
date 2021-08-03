56ea14baefa07f17a68e787f00d3b374c74977ac
SumanManoharShettigar <55957186+SumanManoharShettigar@users.noreply.github.com>
2021-08-03 11:55:10 +0530

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -6,7 +6,7 @@ window.onload=function(){
    console.log(window);
    var opt = {
       margin:      [-0.45,0,0,0],
-      filename:     'myfile.pdf',
+      filename:     'Resume.pdf',
       image:        { type: 'jpeg', quality: 0.98 },
       html2canvas:  { scale: 2 },
       jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
